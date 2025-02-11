5211f495bf82411d4eeda10c10cba60e5f2f721b
Chandrashekhar Fakirpure <hostnextra1@gmail.com>
2025-02-11 11:01:30 +0530

    mobile menu added

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,24 +1,31 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetTitle,
+    SheetTrigger
+} from "@/components/ui/sheet"
+import { AlignRightIcon } from "lucide-react"
+import Image from "next/image"
 import Link from "next/link"
 import { MaxWidthWrapper } from "./max-width-wrapper"
-import Image from "next/image"
 
 export const Navbar = async () => {
     return (
         <nav className="sticky z-50 h-16 inset-0 top-0 w-full bg-gray-50 drop-shadow-sm">
-            <MaxWidthWrapper className="max-w-screen-3xl">
+            <MaxWidthWrapper className="max-w-screen-3xl flex justify-between items-center">
                 <div className="flex h-16 items-center justify-between">
                     <Link href='/' className="flex z-40">
-                        <Image src='/images/logo.png' width={300} height={53} alt="CompiledBox Logo"/>
+                        <Image src='/images/logo.png' width={300} height={53} alt="CompiledBox Logo" />
                         <span className="sr-only">CompiledBox</span>
                     </Link>
                     <div className="hidden h-full lg:flex items-center space-x-4">
-                        <Link href='/pricing' className= 'flex text-sm items-center gap-1 text-gray-800 hover:bg-gray-100 py-1 px-4 rounded-md'>
+                        <Link href='/pricing' className='flex text-sm items-center gap-1 text-gray-800 hover:bg-gray-100 py-1 px-4 rounded-md'>
                             Pricing
                         </Link>
-                        <Link href='/agency' className= 'flex text-sm items-center gap-1 text-gray-800 hover:bg-gray-100 py-1 px-4 rounded-md'>
+                        <Link href='/agency' className='flex text-sm items-center gap-1 text-gray-800 hover:bg-gray-100 py-1 px-4 rounded-md'>
                             Agency
                         </Link>
-                        <Link href='/blog' className= 'flex text-sm items-center gap-1 text-gray-800 hover:bg-gray-100 py-1 px-4 rounded-md'>
+                        <Link href='/blog' className='flex text-sm items-center gap-1 text-gray-800 hover:bg-gray-100 py-1 px-4 rounded-md'>
                             Blog
                         </Link>
                         <Link href='/contact' className="py-2 text-sm px-8 bg-bgdark hover:bg-bgdark/90 text-gray-100 rounded-full">
@@ -26,7 +33,55 @@ export const Navbar = async () => {
                         </Link>
                     </div>
                 </div>
+                <div className='flex md:hidden px-1'>
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <button><AlignRightIcon size={30} /></button>
+                        </SheetTrigger>
+                        <SheetContent side={'left'}>
+                            <SheetTitle>
+                                <Link href='/' className="flex z-40">
+                                    <Image src='/images/logo.png' width={300} height={53} alt="CompiledBox Logo" />
+                                    <span className="sr-only">CompiledBox</span>
+                                </Link>
+                            </SheetTitle>
+
+                            <div className='pt-5'>
+                                <ul className="space-y-3">
+                                    <li>
+                                        <Link href='/pricing' className='flex text-sm items-center gap-1 text-gray-800 hover:bg-gray-100 py-1 px-4 rounded-md'>
+                                            Pricing
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href='/agency' className='flex text-sm items-center gap-1 text-gray-800 hover:bg-gray-100 py-1 px-4 rounded-md'>
+                                            Agency
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href='/blog' className='flex text-sm items-center gap-1 text-gray-800 hover:bg-gray-100 py-1 px-4 rounded-md'>
+                                            Blog
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href='/contact' className="py-2 text-sm px-8 bg-bgdark hover:bg-bgdark/90 text-gray-100 rounded-full">
+                                            Contact
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <div className="flex items-center gap-1 cursor-default hover:bg-accent rounded-sm w-fit text-sm outline-none focus:bg-accent">
+
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </SheetContent>
+                    </Sheet>
+                </div>
             </MaxWidthWrapper>
+
         </nav>
     )
 }
